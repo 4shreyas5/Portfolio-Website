4a72252d078bed9880cefb3dab071f6fba8fd31d
Shreyas <shreyassingh621@gmail.com>
2024-03-14 03:50:20 +0530

    Index.html finished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import { StarsCanvas } from "./canvas"
 
 const Contact = () => {
   const formRef = useRef();
@@ -129,6 +130,7 @@ const Contact = () => {
       >
         <EarthCanvas />
       </motion.div>
+      <StarsCanvas />
     </div>
   );
 };
